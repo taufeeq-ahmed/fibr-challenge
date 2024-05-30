4e0d63b780672aad420862a1910b45cb337df46c
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-30 21:09:25 +0530

    dashboard component added

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './page.module.css';
+
+function Dashboard() {
+  return (
+    <main className={styles.dashboard}>
+      Dashboard
+    </main>
+  );
+}
+
+export default Dashboard;
