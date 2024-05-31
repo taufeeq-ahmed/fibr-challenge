5acf508936e23bfe418e25f574c73627b775cb45
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-31 15:10:36 +0530

    create landing page added

---
+++ app/create-landing-page/page.tsx
@@ -0,0 +1,12 @@
+import CreateLandingPageForm from '@/page-components/page-create-landing-page/create-form'
+import React from 'react'
+
+function CreateLandingPage() {
+  return (
+    <div>
+        <CreateLandingPageForm/>
+    </div>
+  )
+}
+
+export default CreateLandingPage
---
+++ page-components/page-create-landing-page/create-form/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateLandingPageForm() {
+  return (
+    <form action="">
+      This is the form
+    </form>
+  )
+}
+
+export default CreateLandingPageForm
